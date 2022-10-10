523d347c77ce9f99b79e818f99376ca9c17fbf62
Miko Nieminen <miko.nieminen@iki.fi>
2022-10-10 19:23:47 +0200

    Upgrade to Terraform AWS provider 4.x

--- terraform/aws-account/main.tf
+++ terraform/aws-account/main.tf
@@ -50,22 +50,45 @@ locals {
 resource "aws_s3_bucket" "terraform_bucket" {
   # If var.terraform_backend_s3_bucket_name is empty, use default one
   bucket = coalesce(var.terraform_backend_s3_bucket_name, local.default_bucket_name)
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  object_lock_configuration {
-    object_lock_enabled = "Enabled"
-  }
+
+  object_lock_enabled = true
+
   tags = {
     Name = "S3 Remote Terraform State Store"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.terraform_bucket.bucket
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.terraform_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "terraform_bucket_object_lock_configuration" {
+  bucket = aws_s3_bucket.terraform_bucket.bucket
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
   }
 }
 
