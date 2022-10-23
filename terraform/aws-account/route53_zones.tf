31f879a7e290b0c7b424c6b38af76b33a62d2625
Miko Nieminen <miko.nieminen@iki.fi>
2022-10-23 11:19:04 +0200

    Add instructions for defining Route53 zones

---
+++ terraform/aws-account/route53_zones.tf
@@ -0,0 +1,10 @@
+# Define zones here
+# Usually initial definition of a new zone requires
+# manual steps when registering nameservers to the
+# system where the domain is bought. That's why it
+# is better to define zones at the account size while
+# infrastrucutre side defines subdomains and routing.
+
+# resource "aws_route53_zone" "root" {
+#   name = "example.com"
+# }
