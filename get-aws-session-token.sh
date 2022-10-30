ed278332d8a798f65ea0a7a38f6e840dfc22503d
Miko Nieminen <miko.nieminen@iki.fi>
2022-10-30 14:07:45 +0100

    Improve AWS session token script

--- get-aws-session-token.sh
+++ get-aws-session-token.sh
@@ -1,52 +1,56 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 unset ASSUME_ROLE
 
-if [ ! -f ".env" ]; then
-    echo "Could not find $(pwd)/.env file to read configs." >&2
+WORKDIR=$(
+    cd "$(dirname "$0")"
+    pwd -P
+)
+
+if [ ! -f "${WORKDIR}/.env" ]; then
+    echo "Could not find ${WORKDIR}/.env file to read configs." >&2
     exit 1
 fi
 
 help() {
-    echo "Get AWS session tokens for managing the infrastructure" >&2
-    echo
-    echo "This script helps on setting up environment variables to use 'aws-cli'."
-    echo "First it expects that all credentials are stored using KeepassXC and"
-    echo "that you have 'keepassxc-cli' installed."
-    echo "Then you need to have '.env' file with the following entries:"
-    echo
-    echo "KEEPASSXC_FILE=\"<path to keepassxc file that should be used>\""
-    echo "KEEPASSXC_ENTRY=\"<keepassxc entry name holding credentials>\""
-    echo "AWS_PROFILE=\"<profile name in ~/.aws/config or if empty or missing, using default>\""
-    echo
-    echo "In KeepassXC, the entry needs TOTP configured for the entry and then additional"
-    echo "variable defined as 'mfa-device-arn' with value from your AWS account."
-    echo "You can find this value in https://console.aws.amazon.com/iam/home#/security_credentials"
-    echo "under 'Multi-factor authentication (MFA)' where you should have at least one"
-    echo "item under 'Assigned MFA device'"
-    echo
-    echo "When running this script, it will promt for keepassxc password and finally"
-    echo "outputs environment varibles for your new session. You can source these"
-    echo "varibles into your environment by wrapping the script output into an eval:"
-    echo "'eval \$(./get-aws-session-token.sh)'"
-    echo
-    echo
-    echo "Syntax: $0 [-p|r|h]"
-    echo "options:"
-    echo "r     Role to acquire. When left out, provides user specific redentials instead"
-    echo "      of creadentials bound to an assumed role."
-    echo "p     Override profile selection"
-    echo "h     Print this help."
-    echo
+    echo "Tool to get AWS session tokens for managing the infrastructure" >&2
+    echo "" >&2
+    echo "Usage: $(basename "$0") [options...]" >&2
+    echo " -r <role>    Role to acquire. When left out, provides user specific credentials" >&2
+    echo "              instead of creadentials bound to an assumed role. Role should be" >&2
+    echo "              'infra-admin', 'account-admin' or one defined under the AWS account." >&2
+    echo " -p <profile> Override AWS client profile selection" >&2
+    echo " -h           Print this help." >&2
+    echo "" >&2
+    echo "This script helps on setting up environment variables to use 'aws-cli'." >&2
+    echo "First it expects that all credentials are stored using KeepassXC and" >&2
+    echo "that you have 'keepassxc-cli' installed." >&2
+    echo "Then you need to have '.env' file with the following entries:" >&2
+    echo "" >&2
+    echo "KEEPASSXC_FILE=\"<path to keepassxc file to use>\"" >&2
+    echo "KEEPASSXC_ENTRY=\"<keepassxc entry name holding credentials>\"" >&2
+    echo "AWS_PROFILE=\"<profile from ~/.aws/config or use default if empty or missing>\"" >&2
+    echo "" >&2
+    echo "In KeepassXC, the entry needs TOTP configured for the entry and then additional" >&2
+    echo "variable defined as 'mfa-device-arn' with value from your AWS account. You can" >&2
+    echo "find this value in https://console.aws.amazon.com/iam/home#/security_credentials" >&2
+    echo "under 'Multi-factor authentication (MFA)' where you should have at least one" >&2
+    echo "item under 'Assigned MFA device'" >&2
+    echo "" >&2
+    echo "When running this script, it will promt for keepassxc password and finally" >&2
+    echo "outputs environment varibles for your new session. You can source these" >&2
+    echo "varibles into your environment by wrapping the script output into an eval:" >&2
+    echo "'eval \$(./$0)'" >&2
+    echo "" >&2
 }
 
 # Skip checking .env file as it is not under git
-# shellcheck disable=SC1091
-source .env
+# shellcheck source=/dev/null
+. "${WORKDIR}/.env"
 
 AWS_PROFILE="${AWS_PROFILE:-""}"
 AWS_ROLE="${AWS_ROLE:-""}"
