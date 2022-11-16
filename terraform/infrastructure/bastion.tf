e1a915484e49f2d30c71ee0bdd42e5d9e77a0d8d
Miko Nieminen <miko.nieminen@iki.fi>
2022-11-16 22:09:48 +0100

    Add bastion
    This bastion server acts as a jump proxy allowing SSH connections to
    machines in private subnets.
    
    You can SSH into machines in the private network by running:
    ```sh
    ssh -i /path/to/private_key_file -J bastion@bastion_server_public_address ubuntu@private_subnet_address
    ```

--- terraform/infrastructure/bastion.tf
+++ terraform/infrastructure/bastion.tf
@@ -1,52 +1,52 @@
 # uncomment to create bastion server that can be used as a SSH jump proxy
 
-# data "template_cloudinit_config" "bastion" {
-#   gzip          = true
-#   base64_encode = true
+data "template_cloudinit_config" "bastion" {
+  gzip          = true
+  base64_encode = true
 
-#   part {
-#     content_type = "text/cloud-config"
-#     content = templatefile("./files/bastion/cloudinit.cfg.tmpl", {
-#       hostname             = "bastion"
-#       admin_keys           = local.admin_keys
-#       allowed_bastion_keys = local.allowed_bastion_keys
-#     })
-#   }
-# }
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("./files/bastion/cloudinit.cfg.tmpl", {
+      hostname             = "bastion"
+      admin_keys           = local.admin_keys
+      allowed_bastion_keys = local.allowed_bastion_keys
+    })
+  }
+}
 
-# resource "aws_instance" "bastion" {
-#   ami           = local.instances.bastion.ami
-#   instance_type = local.instances.bastion.instance_type
+resource "aws_instance" "bastion" {
+  ami           = local.instances.bastion.ami
+  instance_type = local.instances.bastion.instance_type
 
-#   key_name = var.deployment_key_name
+  key_name = var.deployment_key_name
 
-#   subnet_id = aws_subnet.public_subnet_a.id
+  subnet_id = aws_subnet.public_subnet_a.id
 
-#   user_data = data.template_cloudinit_config.bastion.rendered
+  user_data = data.template_cloudinit_config.bastion.rendered
 
-#   vpc_security_group_ids = [
-#     aws_security_group.bastion.id,
-#   ]
+  vpc_security_group_ids = [
+    aws_security_group.bastion.id,
+  ]
 
-#   # We want to control manually when to recreate these, by using `terraform taint`
-#   lifecycle {
-#     ignore_changes = [ami, user_data, key_name]
-#   }
+  # We want to control manually when to recreate these, by using `terraform taint`
+  lifecycle {
+    ignore_changes = [ami, user_data, key_name]
+  }
 
-#   tags = {
-#     Name = "bastion-${module.env.name}"
-#     Env  = module.env.name
-#   }
-# }
+  tags = {
+    Name = "bastion-${module.env.name}"
+    Env  = module.env.name
+  }
+}
 
-# output "bastion" {
-#   value = {
-#     name            = aws_instance.bastion.tags.Name
-#     instance_id     = aws_instance.bastion.id
-#     state           = aws_instance.bastion.instance_state
-#     public_address  = aws_instance.bastion.public_dns
-#     private_address = aws_instance.bastion.private_dns
-#     az              = aws_instance.bastion.availability_zone
-#     taint_command   = "terraform taint aws_instance.bastion"
-#   }
-# }
+output "bastion" {
+  value = {
+    name            = aws_instance.bastion.tags.Name
+    instance_id     = aws_instance.bastion.id
+    state           = aws_instance.bastion.instance_state
+    public_address  = aws_instance.bastion.public_dns
+    private_address = aws_instance.bastion.private_dns
+    az              = aws_instance.bastion.availability_zone
+    taint_command   = "terraform taint aws_instance.bastion"
+  }
+}
